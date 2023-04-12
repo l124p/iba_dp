93f34c2cb413121944b9066466fa29818b60f4b5
Andry <andry.peshko@gmail.com>
2023-04-12 21:41:17 +0300

    1

--- main.tf
+++ main.tf
@@ -19,59 +19,22 @@ module "aws_ecr_repository"{
    source = "./modules/aws_ecr_repository"
 }
 
-# module "aws_network" {
-#   source = "./modules/aws_network"
-# }
-
-# module "sg"{
-#   source = "./modules/security_group"
-# }
-
-# module "dynamo"{
-#   source = "./modules/dynamo"
-# }
 
 module "aws_network" {
    source = "./modules/deployments"
  }
 
 
-
-
 data "aws_availability_zones" "available" {}
-
-#data "aws_availability_zones" "available" {
-    #state = "available"
-#}
-
-
-# data "aws_eks_cluster" "eks" {
-#   name = module.eks.cluster_id
-# }
-
-# data "aws_eks_cluster_auth" "eks" {
-#   name = module.eks.cluster_id
-# }
 
 
 locals {
   name   = "ex-${replace(basename(path.cwd), "_", "-")}"
-  #name = ""
   region = "us-east-1"
-
-  #vpc_cidr = "10.0.0.0/16"
-  #azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
     Example    = local.name
     GithubRepo = "terraform-aws-eks"
-   # GithubOrg  = "terraform-aws-modules"
   }
 }
 
-# data "terraform_remote_state" "eks" {
-#   backend = "local"
-#   config = {
-#     path = "../eks/terraform.tfstate"
-#   }
-# }
