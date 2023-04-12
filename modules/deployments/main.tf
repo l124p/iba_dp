11d7c23e9dc3620956ed98e44e515e80e2d8f48b
Andry <andry.peshko@gmail.com>
2023-04-12 20:03:14 +0300

    1

--- modules/deployments/main.tf
+++ modules/deployments/main.tf
@@ -21,28 +21,82 @@
 
 
 
-resource "aws_ecs_task_definition" "my_php_app" {
-  family                   = "my-php-app" # Naming our first task
-  container_definitions    = <<DEFINITION
-  [
-    {
-      "name": "php",
-      "image": "097084951758.dkr.ecr.us-east-1.amazonaws.com/web:latest",
-      "essential": true,
-      "portMappings": [
-        {
-          "containerPort": 80,
-          "hostPort": 8080
-        }
-      ],
-      "memory": 512,
-      "cpu": 256
+# resource "aws_ecs_task_definition" "my_php_app" {
+#   family                   = "my-php-app" # Naming our first task
+#   container_definitions    = <<DEFINITION
+#   [
+#     {
+#       "name": "php",
+#       "image": "097084951758.dkr.ecr.us-east-1.amazonaws.com/web:latest",
+#       "essential": true,
+#       "portMappings": [
+#         {
+#           "containerPort": 80,
+#           "hostPort": 8080
+#         }
+#       ],
+#       "memory": 512,
+#       "cpu": 256
+#     }
+#   ]
+#   DEFINITION
+#   requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+#   network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+#   memory                   = 512         # Specifying the memory our container requires
+#   cpu                      = 256         # Specifying the CPU our container requires
+#   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+# }
+
+resource “kubernetes_namespace” “test” {
+  metadata {
+    name = “nginx”
+  }
+}
+
+
+resource “kubernetes_deployment” “test” {
+  metadata {
+    name      = “nginx”
+    namespace = kubernetes_namespace.test.metadata.0.name
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = “MyTestApp”
+      }
     }
-  ]
-  DEFINITION
-  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
-  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
-  memory                   = 512         # Specifying the memory our container requires
-  cpu                      = 256         # Specifying the CPU our container requires
-  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+    template {
+      metadata {
+        labels = {
+          app = “MyTestApp”
+        }
+      }
+      spec {
+        container {
+          image = “nginx”
+          name  = “nginx-container”
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+resource “kubernetes_service” “test” {
+  metadata {
+    name      = “nginx”
+    namespace = kubernetes_namespace.test.metadata.0.name
+  }
+  spec {
+    selector = {
+      app = kubernetes_deployment.test.spec.0.template.0.metadata.0.labels.app
+    }
+    type = “LoadBalancer”
+    port {
+      port        = 80
+      target_port = 80
+    }
+  }
 }
