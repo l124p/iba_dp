5da66fd2a0d4df02746b7fe54a47d78a105135d6
Andry <andry.peshko@gmail.com>
2023-04-12 21:32:35 +0300

    1

--- kubernetes.tf
+++ kubernetes.tf
@@ -21,7 +21,7 @@
 #     command     = "aws"
 #   }
 # }
-
+###
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
