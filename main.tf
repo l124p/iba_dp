36612c1ac1eb3d2fe7cffd081f76b33da0584919
Andry <andry.peshko@gmail.com>
2023-04-12 21:00:51 +0300

    1

--- main.tf
+++ main.tf
@@ -45,13 +45,13 @@ data "aws_availability_zones" "available" {}
 #}
 
 
-data "aws_eks_cluster" "eks" {
-  name = module.eks.cluster_id
-}
+# data "aws_eks_cluster" "eks" {
+#   name = module.eks.cluster_id
+# }
 
-data "aws_eks_cluster_auth" "eks" {
-  name = module.eks.cluster_id
-}
+# data "aws_eks_cluster_auth" "eks" {
+#   name = module.eks.cluster_id
+# }
 
 
 locals {
