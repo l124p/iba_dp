92a5f1711e2f65df0f42fb1c092383b9c0a43f9d
Andry <andry.peshko@gmail.com>
2023-04-12 20:35:49 +0300

    1

--- main.tf
+++ main.tf
@@ -45,6 +45,15 @@ data "aws_availability_zones" "available" {}
 #}
 
 
+data "aws_eks_cluster" "eks" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_id
+}
+
+
 locals {
   name   = "ex-${replace(basename(path.cwd), "_", "-")}"
   #name = ""
