63430aac49a8eacdc14a7d76ffd49fd9b7ac30dc
Andry <andry.peshko@gmail.com>
2023-04-12 20:40:56 +0300

    1

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,8 +1,8 @@
  provider "kubernetes" {
    load_config_file = false
-   host                   = data.aws_eks_cluster.eks-cluster.endpoint
+   host                   = data.aws_eks_cluster.l124-DP-Cluster.endpoint
    #token                  = data.aws_eks_cluster.eks-cluster.token
-   cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority.0.data)
+   cluster_ca_certificate = base64decode(data.aws_eks_cluster.l124-DP-Cluster.certificate_authority.0.data)
    exec {
     api_version = "client.authentication.k8s.io/v1"
     args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
@@ -10,8 +10,8 @@
   }
  }
 provider "kubectl" {
-  host                   = data.aws_eks_cluster.eks-cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority.0.data)
+  host                   = data.aws_eks_cluster.l124-DP-Cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.l124-DP-Cluster.certificate_authority.0.data)
   load_config_file       = false
   exec {
     api_version = "client.authentication.k8s.io/v1"
