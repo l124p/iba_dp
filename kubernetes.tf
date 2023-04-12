ca12af9913c46321a5fa1da8f97ef779e371044f
Andry <andry.peshko@gmail.com>
2023-04-12 21:14:18 +0300

    1

--- kubernetes.tf
+++ kubernetes.tf
@@ -20,9 +20,27 @@
 #   }
 # }
 
+# provider "kubernetes" {
+#   host                   = module.eks.cluster_endpoint
+#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+#   exec {
+#   api_version = "client.authentication.k8s.io/user"
+#   args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+#   command = "aws"
+#   }
+#   # exec {
+#   #   api_version = "client.authentication.k8s.io/v1beta1"
+#   #   command     = "aws"
+#   #   # This requires the awscli to be installed locally where Terraform is executed
+#   #   args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+#   # }
+# }
+
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  cluster_ca_certificate = base64decode(module.eks.certificate_authority.0.data)
 
   exec {
   api_version = "client.authentication.k8s.io/user"
