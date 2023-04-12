808ce085e4b2ba71f9cddd8f6475c26cc7e76f73
Andry <andry.peshko@gmail.com>
2023-04-12 20:08:30 +0300

    1

--- modules/deployments/main.tf
+++ modules/deployments/main.tf
@@ -21,35 +21,35 @@
 
 
 
-resource "aws_ecs_task_definition" "my_php_app" {
-  family                   = "my-php-app" # Naming our first task
-  container_definitions    = <<DEFINITION
-  [
-    {
-      "name": "php",
-      "image": "097084951758.dkr.ecr.us-east-1.amazonaws.com/web:latest",
-      "essential": true,
-      "portMappings": [
-        {
-          "containerPort": 80,
-          "hostPort": 8080
-        }
-      ],
-      "memory": 512,
-      "cpu": 256
-    }
-  ]
-#   DEFINITION
-#   requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
-#   network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
-#   memory                   = 512         # Specifying the memory our container requires
-#   cpu                      = 256         # Specifying the CPU our container requires
-#   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
-}
+# resource "aws_ecs_task_definition" "my_php_app" {
+#   family                   = "my-php-app" # Naming our first task
+#   container_definitions    = <<DEFINITION
+#   [
+#     {
+#       "name": "php",
+#       "image": "097084951758.dkr.ecr.us-east-1.amazonaws.com/web:latest",
+#       "essential": true,
+#       "portMappings": [
+#         {
+#           "containerPort": 80,
+#           "hostPort": 8080
+#         }
+#       ],
+#       "memory": 512,
+#       "cpu": 256
+#     }
+#   ]
+# #   DEFINITION
+# #   requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+# #   network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+# #   memory                   = 512         # Specifying the memory our container requires
+# #   cpu                      = 256         # Specifying the CPU our container requires
+# #   execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+# }
 
 resource "kubernetes_namespace" "test" {
    metadata {
-     name = “nginx”
+     name = "nginx"
    }
 }
 
