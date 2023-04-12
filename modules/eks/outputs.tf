2f2575cd6c1aa7320358c5a74942d5945d61669a
Andry <andry.peshko@gmail.com>
2023-04-12 20:54:05 +0300

    1

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,14 +1,14 @@
+output "cluster_endpoint" {
+  description = "Endpoint for your Kubernetes API server"
+  value       = module.eks.cluster_endpoint
+}
+
 output "cluster_id" {
   description = "The ID of the EKS cluster. Note: currently a value is returned only for local EKS clusters created on Outposts"
-  value       = try(eks.this[0].cluster_id, "")
+  value       = module.eks.cluster_id
 }
 
 output "cluster_name" {
   description = "The name of the EKS cluster"
-  value       = try(eks.this[0].name, "")
-}
-
-output "cluster_endpoint" {
-  description = "Endpoint for your Kubernetes API server"
-  value       = try(eks.this[0].endpoint, null)
+  value       = module.eks.cluster_name
 }
