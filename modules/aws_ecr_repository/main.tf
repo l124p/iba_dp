8a76bc1d9bbca8029f7e4be2289137145351babf
Andry <andry.peshko@gmail.com>
2023-04-13 20:06:13 +0300

    update

--- modules/aws_ecr_repository/main.tf
+++ modules/aws_ecr_repository/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "l124_dp" {
-  name                 = "web"
-  # image_scanning_configuration {
-  #   scan_on_push = true
-  # }
+  name                 = "php-web"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 }
