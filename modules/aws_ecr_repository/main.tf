7a9ffbb66e6ef6fc956bfb7a27c1c92c88da6d92
andry <andry.peshko@gmail.com>
2023-04-13 16:46:21 +0300

    update

--- modules/aws_ecr_repository/main.tf
+++ modules/aws_ecr_repository/main.tf
@@ -1,6 +1,6 @@
 resource "aws_ecr_repository" "l124_dp" {
   name                 = "app"
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+  # image_scanning_configuration {
+  #   scan_on_push = true
+  # }
 }
